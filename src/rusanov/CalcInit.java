6cf8d323f728fbab42e381982745920c5d262929
RomanRusanov <roman_81@mail.ru>
2016-05-31 17:51:36 +0400

    Update CalcInit.java

--- src/rusanov/CalcInit.java
+++ src/rusanov/CalcInit.java
@@ -17,14 +17,5 @@ public class CalcInit {
 		//
 		calc.multiple(5,5);
 		System.out.printf("%d * %d = %d", first, second, result);
-	
-=======
-	public static void main(String[] arg); {
-		Calculator calculator = new Calculator();
-		calculator.add(5,5);
-		calculator.subtract(5,5);
-		calculator.div(5,5);
-		calculator.multiple(5,5);
->>>>>>> 4b99a54face90170d32434ea4ace94e2742bcf36
 	}
 }
