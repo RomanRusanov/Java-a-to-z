ee147230405f9712c45f1d3a1f2ca2f3738c614e
Роман Русанов <roman@MacBookPro.local>
2016-06-01 10:51:03 +0300

    Edit

---
+++ Src/Rusanov/test.java
@@ -0,0 +1,13 @@
+package rusanov;
+
+public class test {
+
+	public static void main(String[] args) {
+	Integer first = 0;
+	Integer second = 0;
+	Integer result = 0;
+		
+		System.out.printf("%d + %d = %d", first, second, result);
+		
+	}
+}
