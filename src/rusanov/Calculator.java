01be9248840d03ff971c06fe0427a30a8202b280
Роман Русанов <roman@MacBookPro.local>
2016-06-01 11:10:01 +0300

    Edit

--- src/rusanov/Calculator.java
+++ src/rusanov/Calculator.java
@@ -2,21 +2,23 @@ package rusanov;
 
 public class Calculator {
 
+    private Integer first, second;	
+
     private double result = 0;
 		
 	public void add(double first, double second) {
-		this.result = first + second;		
+		this.result = this.first + this.second;		
 	}
 	
 	public void subtract(double first, double second) {
-		this.result = first - second;
+		this.result = this.first - this.second;
 	}
 	
 	public void div(double first, double second) {
-		this.result = first / second;
+		this.result = this.first / this.second;
 	}
 	
 	public void multiple(double first, double second) {
-		this.result = first * second;
+		this.result = this.first * this.second;
 	}
 }
