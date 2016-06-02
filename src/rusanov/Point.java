9d71eb4d0f3ae716286fe2ad0b4d6a708bc0d88a
RomanRusanov <roman_81@mail.ru>
2016-06-02 18:16:37 +0300

    Edit

--- src/rusanov/Point.java
+++ src/rusanov/Point.java
@@ -1,17 +1,26 @@
 package rusanov
 
 //*
- * Class Класс для установления точки по двум координатам
+ * Class Класс для установления точки по двум координатам, вычисление расстояния между ними.
  * @author rusanov
+ * @param x,y (point)
+ * @param distanceTo (вычисление растояния между точками)
  * @since 02.06.2016
  * @version 0.1
  *//
+ 
 public class Point {
+
 	public double x,y;
-		
+	
+	//* метод Point*//	
 	public Point (double x, double y) {
 		this.x = x;
 		this.y = y;
-		}
+	}
 	
-};
+	//* вычисление расстояния между точками*//
+	public double distanceTo (Point Point) {
+	
+	}
+}
