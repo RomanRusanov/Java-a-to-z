2bdb1198d724f4052fe5edb0bed968a3abb3fb97
RomanRusanov <roman_81@mail.ru>
2016-06-02 17:53:26 +0300

    Remove files to final folder

---
+++ src/rusanov/Point.java
@@ -0,0 +1,17 @@
+package rusanov
+
+//*
+ * Class  ласс дл€ установлени€ точки по двум координатам
+ * @author rusanov
+ * @since 02.06.2016
+ * @version 0.1
+ *//
+public class Point {
+	public double x,y;
+		
+	public Point (double x, double y) {
+		this.x = x;
+		this.y = y;
+		}
+	
+};
