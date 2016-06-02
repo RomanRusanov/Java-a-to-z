33efec37612b333e74245d86547dcf7fb53b1fdc
RomanRusanov <roman_81@mail.ru>
2016-06-02 15:11:28 +0300

    add Example, Exercises

---
+++ src/rali/ShowConditions.java
@@ -0,0 +1,9 @@
+package rali;
+
+public class ShowConditions {
+	public static void main(String[] args) {
+		boolean condition = false;
+		System.out.println(condition ? "this is true":"this is false");
+
+	}
+}	
