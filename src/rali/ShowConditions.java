8ece14b8b4b317055b1a245666d14fddf4493041
Роман Русанов <roman@MacBookPro.local>
2016-10-24 17:56:24 +0300

    clean

--- src/rali/ShowConditions.java
+++
@@ -1,9 +0,0 @@
-package rali;
-
-public class ShowConditions {
-	public static void main(String[] args) {
-		boolean condition = false;
-		System.out.println(condition ? "this is true":"this is false");
-
-	}
-}	
