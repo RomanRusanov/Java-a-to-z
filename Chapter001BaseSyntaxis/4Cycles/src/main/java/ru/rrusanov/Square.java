9c614027e337f06f0756cfd400bcd0acb92de55e
Roman Rusanov <roman_81@mail.ru>
2016-11-14 21:41:34 +0300

    Edited Square.java, SquareTest.java

--- Chapter001BaseSyntaxis/4Cycles/src/main/java/ru/rrusanov/Square.java
+++ Chapter001BaseSyntaxis/4Cycles/src/main/java/ru/rrusanov/Square.java
@@ -1,36 +1,40 @@
-/**
- * Class calculate ( a * x2 + b * x + c ) and show how change value 
- * if x = (start...finish) with step 
- * @author Roman Rusanov
- * @param int x
- * @param int start - (first value of x)
- * @param int finish - (final value of x)
- * @param int step - (value step between x...x+1)
- * @since 13.11.2016
- * @version 0.1
-**/
-package ru.rrusanov;
-
+package ru.rrusanov.Square;
+/** Class calculate (a*x2+b*x+c) and show how change value if x =(start...finish) with step.
+* @author Roman Rusanov
+* @param int x
+* @param int start - first value of x
+* @param int finish - final value of x
+* @param int step - value step between x...x+1
+* @since 13.11.2016
+* @version 0.1
+*/
 public class Square {
-
-	// Calculate return float ( a * x2 + b * x + c )
+	/** Calculate expression (a*x2+b*x+c).
+	* @param x - int x
+	* {@value} a - a
+	* {@value} b - b
+	* {@value} c - c
+	* @return float - result of expression a*x2+b*x+c
+	*/
 	public float calculate(int x) {
-
-		int a = 2;
-		int b = 3;
-		int c = 4;
-
-		return (float)(a * (x * x) + b * x + c);
+		final int a = 2;
+		final int b = 3;
+		final int c = 4;
+		return (float)  (a * (x * x) + b * x + c);
 	}
 
-	// Show result calculate where x betwen start...finish with step
+	/** Show result calculate where x betwen start...finish with step.
+	 * @param start - int first value of x
+	 * @param finish - int last value of x
+	 * @param step - int step the value increases at each iteration
+	 */
 	public void show(int start, int finish, int step) {
-		/*int i = start;
+		/*
 		for (int start; start < finish; start = start + step ) {
 			System.out.println("y = " + calculate(start) + " x value = " + start);
 		}
 		*/
-		while (start<finish) {
+		while (start < finish) {
 			System.out.println("y = " + calculate(start) + " x value = " + start);
 			start = start + step;
 		}
--- Chapter001BaseSyntaxis/4Cycles/src/test/java/SquareTest.java
+++ Chapter001BaseSyntaxis/4Cycles/src/test/java/SquareTest.java
@@ -1,17 +1,28 @@
 package ru.rrusanov;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+/** Class test Square.java.
+* @author Roman Rusanov
+* @since 13.11.2016
+* @version 0.1
+*/
 public class SquareTest {
-
+	/**Then x=0 When calculate return 4.
+	*/
 	@Test
-	public void thenX0WhenReturnC () {
+	public void thenX0WhenReturn4() {
 		Square square = new Square();
-		assertEquals(4,square.calculate(0),0.0);
+		final float expect = 4.0;
+		final float result = square.calculator(0);
+		assertThat(result, is(expect));
 	}
-
+	/**Then x=2 calculate return 18.
+	*/
 	@Test
-	public void thenX2WhenReturn26 () {
+	public void thenX2WhenReturn18() {
 		Square square = new Square();
-		assertEquals(18,square.calculate(2),0.0);
+		final float expect = 18.0;
+		final float result = square.calculator(2);
+		assertThat(result, is(expect));
 	}
 }
