7ae58f6342fe88a3985fa59be0660dd8babdb523
Roman Rusanov <roman_81@mail.ru>
2016-11-22 09:42:37 +0300

    Exercise 4.3. To build a pyramid in pseudographics.

---
+++ Chapter001BaseSyntaxis/4Cycles/src/test/java/PaintTest.java
@@ -0,0 +1,20 @@
+package ru.rrusanov;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/* Class test Paint.java.
+ * @author Roman Rusanov
+ * @since 22.11.2016
+ * @Version 0.1
+ */
+public class PaintTest {
+	/** Then Heght of piramid 1 return single string
+	*/
+	//@Test
+	public void thenHeghtPiramid1ReturnSingleString() {
+		Paint paint = new Paint();
+		final String expect = "^";
+		final String result = paint.piramid(1);
+		assertThat(result, is(expect));
+	}
+}
