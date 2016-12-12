7bc4e1680e3ba0339f82c4a90da540d3f2de72c3
Roman Rusanov <roman_81@mail.ru>
2016-12-12 08:04:55 +0300

    Added new exercies 5.3 for code review

--- Chapter001BaseSyntaxis/5Array/src/main/java/ru/rrusanov/FindAndDelete.java
+++ Chapter001BaseSyntaxis/5Array/src/main/java/ru/rrusanov/FindAndDelete.java
@@ -9,23 +9,21 @@ public class FindAndDelete {
 	/**
 	 * Find in array duplicated elements.
 	 * @param array - (String[]) for rotate.
-	 * @{value} i,i1 - (int) index for loop.
+	 * @{value} i,j,i1 - (int) index for loop.
 	 * @return array - (String[]) array without duplicate.
 	**/
 	public String[] stringArray(String[] array) {
-		int duplicateCount = 0;
 		for (int i = 0; i < array.length - 1; i++) {
-			for (int j = i + 1; j < array.length - 1; j++) {
-
-				if (array[i].equals(array[i < array.length - 2 ? j : i + 1])) {
-					duplicateCount++;
-					System.out.println(duplicateCount);
+			for (int j = i + 1; j < array.length - 2; j++) {
+				while (array[i].equals(array[j])) {
 					for (int i1 = j; i1 < array.length - 1; i1++) {
 						array[i1] = array[i1 + 1];
 					}
+					array = copyOf(array, array.length - 1);
 				}
 			}
 		}
-		return copyOf(array, array.length);
+		array = copyOf(array, array.length - 1);
+		return array;
 	}
 }
--- Chapter001BaseSyntaxis/5Array/src/test/java/ru/rrusanov/FindAndDeleteTest.java
+++ Chapter001BaseSyntaxis/5Array/src/test/java/ru/rrusanov/FindAndDeleteTest.java
@@ -2,9 +2,9 @@ package ru.rrusanov;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-/** Class test Rotate.java.
+/** Class test FindAndDelete.java.
  * @author Roman Rusanov
- * @since 5.12.2016
+ * @since 7.12.2016
  * @version 0.1
 **/
 public class FindAndDeleteTest {
@@ -14,7 +14,7 @@ public class FindAndDeleteTest {
 	 **/
 	@Test
 	public void thenArrayHaveDuplicateElementsWhenReturnArrayWithoutDuplicate() {
-		final String[] array = {"Hello", "Hello", "my", "name", "name", "is", "is", "Nute", "Hello", "Hello"};
+		final String[] array = {"Hello", "Hello", "Hello", "my", "name", "name", "is", "is", "Nute", "is"};
 		FindAndDelete findAndDelete = new FindAndDelete();
 		final String[] expectArray = {"Hello", "my", "name", "is", "Nute"};
 		final String[] resultArray = findAndDelete.stringArray(array);
