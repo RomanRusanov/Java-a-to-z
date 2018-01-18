9437f9fe37b61678c2790c76427da51acc6c3fc4
RusanovRoman <rusanov_roman@icloud.com>
2018-01-18 16:21:48 +0300

    New exercise convert List to Map. Send to code review.

---
+++ Chapter003_CollectionLite/src/main/java/ru/rrusanov/ListToMap/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 18.01.2018
+ *
+ * UserConvert class make the conversion List to Map collection.
+ * Methods:
+ *   HashMap<Integer,User> process(List<User> list)
+ *      Take list of user and convert into map with key Integer for user with that key.
+ * User class describes User.
+ * Methods:
+ *   int generateId() Generate random id.
+ *   int getId() Getter for id.
+ */
+package ru.rrusanov.ListToMap;
---
+++ Chapter003_CollectionLite/src/test/java/ru/rrusanov/ListToMap/UserConvertTest.java
@@ -0,0 +1,36 @@
+package ru.rrusanov.ListToMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 18.01.2018
+ *
+ * Class test UserConvert class.
+ */
+public class UserConvertTest {
+    /**
+     * Then pass List<User> collection return HashMap<Id, User> collection.
+     */
+    @Test
+    public void thenPassListWhenReturnHashMap() {
+        List<User> list = new ArrayList<>(Arrays.asList(new User("Ivan", "Tombov"),
+                                                        new User("Fedor", "Omsk"),
+                                                        new User("Andrei", "Voronez"))
+        );
+        UserConvert userConvert = new UserConvert();
+        HashMap<Integer, User> hashMap = userConvert.process(list);
+        int expect = list.get(0).getId();
+        User result = hashMap.get(expect);
+        Assert.assertThat(result.getId(), is(expect));
+    }
+}
