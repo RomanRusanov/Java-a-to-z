402c3262cae93d661ac8b417b089f40b8dddba0b
Roman Rusanov <roman_81@mail.ru>
2018-01-22 22:13:58 +0300

    New exercise create comparator and compare all elements into two collection List<Integer>.

---
+++ Chapter003_CollectionLite/src/main/java/ru/rrusanov/ListCompare/ListCompare.java
@@ -0,0 +1,45 @@
+package ru.rrusanov.ListCompare;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Class compare to List<Integer> by each element..
+ *
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 22.01.2018
+ */
+public class ListCompare implements Comparator<List<Integer>> {
+    /**
+     * Compare two Integer List by each element.
+     * If lists equal return 0;
+     * If left List less return -1;
+     * if left bigger return 1;
+     * @param left first List of Integer.
+     * @param right second List of Integer.
+     * @return int value result comparision (0,1,-1).
+     */
+    @Override
+    public int compare(List<Integer> left, List<Integer> right) {
+        int result = 0; // default state.
+        int leftSize = left.size();
+        int rightSize = right.size();
+        if (leftSize < rightSize) {
+            result = -1;
+        } else if (leftSize > rightSize) {
+            result = 1;
+        } else if (leftSize == rightSize) {
+            for (int i = 0; i < leftSize; i++) {
+                if (left.get(i) < right.get(i)) {
+                    result = -1;
+                    break;
+                } else if (left.get(i) > right.get(i)) {
+                    result = 1;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ Chapter003_CollectionLite/src/test/java/ru/rrusanov/ListCompare/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 22.01.2018
+ *
+ * ListCompareTest.java Class test ListCompare class.
+ * Methods:
+ *   void whenLeftAndRightEqualsThenZero() When all equals return 0.
+ *   void whenLeftLessRightThenMinis() When left List elements less right when return -1.
+ *   void whenLeftGreatRightThenPlus() When left List element bigger right when return 1.
+ */
+package ru.rrusanov.ListCompare;
