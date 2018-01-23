9ce9f98dd72ade2d23ceeba77833b0e1e4259a7c
RusanovRoman <rusanov_roman@icloud.com>
2018-01-23 10:56:47 +0300

    Edited files to correct build all project Java-a-to-z.

--- Chapter001_BaseSyntaxis/4Cycles/src/main/java/ru/rrusanov/Factorial.java
+++ Chapter001_BaseSyntaxis/4Cycles/src/main/java/ru/rrusanov/Factorial.java
@@ -1,25 +1,30 @@
 package ru.rrusanov;
-/** Exersice 4.2 Create a program that calculates the factorial.
+
+/**
  * @author Roman Rusanov
- * @since 19.11.2016
  * @version 0.1
+ * @since 19.11.2016
+ *
+ * Exercise 4.2 Create a program that calculates the factorial.
  */
 public class Factorial {
-	/** The calculates the value of the function factorial.
-	 * @param n - the last value(int) up to which to calculate the factorial of
-	 * @return result - the value(long) of the function factorial
-	 * {@value} i - index of iteration for loop
-	 */
-	public long calculate(int n) {
-		if (n > 0) {
-			long result = 1;
-			for (int i = 1; i <= n; i++ ) {
-				result *= i;
-			}
-			return result;
-		} else {
-			System.out.println("ERROR! Function factorial can be calculated only for positive integers.");
-			return -1;
-		}
-	}
+    /**
+     * The calculates the value of the function factorial.
+     *
+     * @param n - the last value(int) up to which to calculate the factorial of
+     * @return result - the value(long) of the function factorial
+     * {@value} i - index of iteration for loop
+     */
+    public long calculate(int n) {
+        if (n > 0) {
+            long result = 1;
+            for (int i = 1; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        } else {
+            System.out.println("ERROR! Function factorial can be calculated only for positive integers.");
+            return -1;
+        }
+    }
 }
--- Chapter001_BaseSyntaxis/4Cycles/src/test/java/ru/rrusanov/PaintTest.java
+++ Chapter001_BaseSyntaxis/4Cycles/src/test/java/ru/rrusanov/PaintTest.java
@@ -2,18 +2,20 @@ package ru.rrusanov;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-/* Class test Paint.java.
+/** Class test Paint.java.
  * @author Roman Rusanov
  * @since 22.11.2016
- * @Version 0.1
+ * @version 0.1
  */
 public class PaintTest {
-	//Then Heght of piramid 1 return single string
+	/**
+	 * Then height of pyramid 1 return single string.
+	 */
 	@Test
 	public void thenHeghtPiramid1ReturnSingleString() {
 		Paint paint = new Paint();
 		final String expect = " ^\n";
 		final String result = paint.piramid(1);
-		assertThat(result, is(expect));	
+		assertThat(result, is(expect));
 	}
 }
