8a4f9e36d3534b9d862631bb98db79634e7fff2b
RusanovRoman <rusanov_roman@icloud.com>
2018-02-08 15:09:23 +0300

    New exercise "Sort departments".

---
+++ Chapter003_CollectionLite/src/main/java/ru/rrusanov/SortDepartment/Sort.java
@@ -0,0 +1,19 @@
+package ru.rrusanov.SortDepartment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The class sorts departments in flowing order.
+ * K 1...n, SK 1...n, SSK 1...n. - Natural ordering
+ */
+public class Sort {
+    /**
+     * Sort by ascending order.
+     */
+    public List<String> ascendingOrder(String[] unsorted) {
+        ArrayList<String> result;
+
+        return result;
+    }
+}
