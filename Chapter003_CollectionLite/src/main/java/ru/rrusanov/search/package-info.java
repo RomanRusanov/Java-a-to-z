600cbda35030bab8cccd130635176daf07dc6db3
RusanovRoman <rusanov_roman@icloud.com>
2018-03-02 15:16:37 +0300

    New exercise phoneDictionary by use ArrayList.

---
+++ Chapter003_CollectionLite/src/main/java/ru/rrusanov/search/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 02.03.2018
+ *
+ * package ru.rrusanov.search
+ *
+ * Person.java Class define Person.
+ *  String getName() Getter for field name.
+ *  String getSurname() Getter for field surname.
+ *  String getPhone() Getter for field phone.
+ *  String getAddress() Getter for field address.
+ *
+ * PhoneDictionary.java Class define PhoneDictionary.
+ *  List<Person> getPersons() Getter fot dictionary collection.
+ *   void add(Person person) Add person to dictionary.
+ *   List<Person> find(String key) Вернуть список всех пользователей, который содержат key в любых полях.
+ */
+package ru.rrusanov.search;
---
+++ Chapter003_CollectionLite/src/test/java/ru/rrusanov/search/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 02.03.2018
+ *
+ * package ru.rrusanov.search
+ *
+ * PersonTest.java Class test Person class.
+ *  void thenNewPersonCreatedWhenNameNotNull() Test for method getName.
+ *  void thenNewPersonCreatedWhenSurnameNotNull() Test for method getSurname.
+ *  void thenNewPersonCreatedWhenPhoneNotNull() Test for method getPhone.
+ *  void thenNewPersonCreatedWhenAddressNotNull() Test for method getAddress.
+ *
+ * PhoneDictionaryTest.java Class test PhoneDictionary class.
+ *  void thenAddedPersonWhenCollectionNotEmpty() Test for add method.
+ *  void whenFindByName() Test for find method.
+ */
+package ru.rrusanov.search;
