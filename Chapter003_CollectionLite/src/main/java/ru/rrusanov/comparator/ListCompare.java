e204705b28860f194670f6d34e370a03e8cef168
RomanRusanov <rusanov_roman@icloud.com>
2018-03-04 21:59:18 +0300

    New exercise ListComparator.

---
+++ Chapter003_CollectionLite/src/main/java/ru/rrusanov/comparator/ListCompare.java
@@ -0,0 +1,24 @@
+package ru.rrusanov.comparator;
+import java.util.Comparator;
+
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 04.03.2018
+ *
+ * Class define ListCompare.
+ */
+public class ListCompare implements Comparator<String>{
+    /**
+     * Method compare strings
+     * @param left String
+     * @param right String
+     * @return result operation compare. Return 1 if left advice right, otherwise -1 and zero if equal.
+     */
+    @Override
+    public int compare(String left, String right) {
+        int result;
+        
+        return result;
+    }
+}
---
+++ Chapter003_CollectionLite/src/main/java/ru/rrusanov/comparator/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 02.03.2018
+ *
+ * package ru.rrusanov.comparator
+ *
+ *
+ */
+package ru.rrusanov.comparator;
