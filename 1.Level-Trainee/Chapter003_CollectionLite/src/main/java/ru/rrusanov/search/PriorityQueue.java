adbb56082b3c85e1e12e43791080fac9ae6f93ef
RomanRusanov <rusanov_roman@icloud.com>
2018-03-19 21:54:34 +0300

    Change end line symbol to LF in PriorityQueue.java

--- 1.Level-Trainee/Chapter003_CollectionLite/src/main/java/ru/rrusanov/search/PriorityQueue.java
+++ 1.Level-Trainee/Chapter003_CollectionLite/src/main/java/ru/rrusanov/search/PriorityQueue.java
@@ -1,1 +1,41 @@
-package ru.rrusanov.search;import java.util.LinkedList;/** * @author Roman Rusanov * @version 0.1 * @since 03.03.2018 * * Class define PriorityQueue. */public class PriorityQueue {    /**     * Collection contain all task.     */    private LinkedList<Task> tasks = new LinkedList<>();    /**     * Метод должен вставлять в нужную позицию элемент.     * Позиция определять по полю приоритет.     * Для вставик использовать add(int index, E value)     * @param task задача     */    public void put(Task task) {        boolean positionFind = false;        for (int i = 0; i < this.tasks.size(); i++) {            if (task.getPriority() < this.tasks.get(i).getPriority()) {                this.tasks.add(i, task);                positionFind = true;                break;            }        }        if (!positionFind) {            this.tasks.add(this.tasks.size(), task);        }    }    /**     * Get last task and remove from collection.     * @return Task.     */    public Task take() {        return this.tasks.poll();    }}
+package ru.rrusanov.search;
+import java.util.LinkedList;
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 03.03.2018
+ *
+ * Class define PriorityQueue.
+ */
+public class PriorityQueue {
+    /**
+     * Collection contain all task.
+     */
+    private LinkedList<Task> tasks = new LinkedList<>();
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        boolean positionFind = false;
+        for (int i = 0; i < this.tasks.size(); i++) {
+            if (task.getPriority() < this.tasks.get(i).getPriority()) {
+                this.tasks.add(i, task);
+                positionFind = true;
+                break;
+            }
+        }
+        if (!positionFind) {
+            this.tasks.add(this.tasks.size(), task);
+        }
+    }
+    /**
+     * Get last task and remove from collection.
+     * @return Task.
+     */
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
