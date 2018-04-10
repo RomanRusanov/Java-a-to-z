eec6f732225bd4810803022e56a0e419d6d184a7
RomanRusanov <rusanov_roman@icloud.com>
2018-04-10 12:05:56 +0300

    Send to code review exercise(Converter for Iterator) package ru.rrusanov.iteratorConverter

--- 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/iteratorConverter/Converter.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/iteratorConverter/Converter.java
@@ -1,7 +1,7 @@
 package ru.rrusanov.iteratorConverter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
  * @author Roman Rusanov
  * @version 0.1
@@ -10,28 +10,71 @@ import java.util.NoSuchElementException;
  * Class convert object Iterator<Iterator<Integer>> to Iterator<Integer>
  */
 public class Converter {
-
+    /**
+     * Field contain Iterator of iterators.
+     */
+    private ArrayList<Iterator<Integer>> itIn = new ArrayList<>();
+    /**
+     * Field pointer to current iterator.
+     */
+    private int position = 0;
+    /**
+     * Method fill list of iterators.
+     * @param iterators to conversion.
+     */
+    public void fillList(Iterator<Iterator<Integer>> iterators) {
+        while (iterators.hasNext()) {
+            this.itIn.add(iterators.next());
+        }
+    }
+    /**
+     * Method convert iterator(iterators(Integer)) to iterator(integer) including all in iterators elements.
+     * @param it iterator to conversion.
+     * @return converted iterator.
+     */
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
-
+        /**
+         * new anonymous class describes iterator.
+         */
         return new Iterator<Integer>() {
-
-            Iterator<Integer> itIn = it.next();
-
+            /**
+             * Method check has iterator next element.
+             * @return true if element exist, otherwise false.
+             */
             @Override
             public boolean hasNext() {
-                return it.hasNext() || itIn.hasNext();
-            }
-
-            @Override
-            public Integer next() {
-                int result;
-                while (true) {
-                    if (itIn.hasNext()) {
-                        result = itIn.next();
+                fillList(it);
+                boolean result = false;
+                while (position < itIn.size()) {
+                    if (itIn.get(position).hasNext()) {
+                        result = true;
                         break;
                     } else {
-                        itIn = it.next();
+                        position++;
                     }
+                }
+                return result;
+            }
+
+            /**
+             * Method return next element.
+             * @return Next Integer element.
+             * @throws NoSuchElementException if no more element in iterator.
+             */
+            @Override
+            public Integer next() throws NoSuchElementException {
+                fillList(it);
+                int result = -1;
+                while (position < itIn.size()) {
+                    if (itIn.get(position).hasNext()) {
+                        result = itIn.get(position).next();
+                        break;
+                    } else {
+                        position++;
+                    }
+                }
+                if (result == -1) {
+                    throw new NoSuchElementException();
                 }
                 return result;
             }
