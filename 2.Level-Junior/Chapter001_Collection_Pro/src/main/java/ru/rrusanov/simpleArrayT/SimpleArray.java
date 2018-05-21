f2c215f439c87d946451cb3202f5a4df288fb3a1
RusanovRoman <rusanov_roman@icloud.com>
2018-05-21 13:18:53 +0300

    Send to review SimpleArrayT.

--- 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/simpleArrayT/SimpleArray.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/simpleArrayT/SimpleArray.java
@@ -1,93 +1,82 @@
 package ru.rrusanov.simpleArrayT;
-
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-
 /**
  * @author Roman Rusanov
  * @version 0.1
  * @since 20.05.2018
  *
  * SimpleArray class wrapper on array.
+ * @param <T> generic type, that collection be contain.
  */
 public class SimpleArray<T>  implements Iterable<T> {
-
-    private T models[];
-
+    /**
+     * The field is contain collection elements.
+     */
+    private ArrayList<T> models;
+    /**
+     * The index is counter of elements collection.
+     */
     private int index = 0;
-
-
+    /**
+     * Default constructor of class.
+     */
+    public SimpleArray() {
+        this.models = new ArrayList<>(5);
+    }
+    /**
+     * The method add element to collection.
+     * @param model element to add.
+     */
     public void add(T model) {
-        if (index < 5) {
-            models[index++] = model;
+        if (index >= 0 && index < 5) {
+            models.add(model);
+            index++;
         } else {
             throw new ArrayIndexOutOfBoundsException("Max models = 5");
         }
     }
+    /**
+     * The method set element in collection.
+     * @param index of element to change.
+     * @param model new element to insert.
+     */
     public void set(int index, T model) {
-        if (index < 5) {
-            models[index] = model;
+        if (index >= 0 && index < 5) {
+            models.set(index, model);
         } else {
             throw new ArrayIndexOutOfBoundsException("Index model out of range");
         }
     }
+    /**
+     * The method delete element from collection, replace value to null.
+     * @param index to delete.
+     */
     public void delete(int index) {
-        if (index < 5) {
-            models[index] = null;
+        if (index >= 0 && index < 5) {
+            models.set(index, null);
         } else {
             throw new ArrayIndexOutOfBoundsException("Index model out of range");
         }
     }
+    /**
+     * The method get value from collection.
+     * @param index element that return method.
+     * @return value of that index.
+     */
     public T get(int index) {
-        if (index < 5) {
-            return models[index];
+        if (index >= 0 && index < 5) {
+            return models.get(index);
         } else {
             throw new ArrayIndexOutOfBoundsException("Index model out of range");
         }
     }
-
+    /**
+     * The method return iterator for collection.
+     * @return iterator.
+     */
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            /**
-             * Position next;
-             */
-            int position = 0;
-            /**
-             * Method return true if Iterator contain Integer values left, otherwise false.
-             * @return boolean
-             */
-            @Override
-            public boolean hasNext() {
-                boolean result = false;
-                while (position < 5) {
-                    result = true;
-                }
-                return result;
-            }
-            /**
-             * Method return next Integer element in iterator.
-             * @return Integer.
-             */
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return models[position++];
-            }
-        };
-    }
-
-    @Override
-    public void forEach(Consumer<? super T> action) {
-
-    }
-
-    @Override
-    public Spliterator<T> spliterator() {
-        return null;
+        return models.iterator();
     }
 }
