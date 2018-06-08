b5d86fba2fcfd3ad0511445bf2b52061dc54dcbb
RusanovRoman <rusanov_roman@icloud.com>
2018-06-08 14:31:24 +0300

    New exercise (Create dynamic list based on array)

---
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/dynamicListBasedArray/DynamicList.java
@@ -0,0 +1,34 @@
+package ru.rrusanov.dynamicListBasedArray;
+
+import java.util.Iterator;
+
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 07.06.2018
+ *
+ * Dynamic List based on object array.
+ * @param <E> generic type.
+ */
+public class DynamicList<E> implements Iterable<E> {
+
+    Object[] container;
+
+    void add(E value) {
+
+    }
+
+    E get(int index) {
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
---
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/test/java/ru/rrusanov/dynamicListBasedArray/DynamicListTest.java
@@ -0,0 +1,25 @@
+package ru.rrusanov.dynamicListBasedArray;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DynamicListTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void iterator() {
+    }
+}
