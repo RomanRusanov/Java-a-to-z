0674771d6c571f17e0a9cb142aa7e029e9744743
RusanovRoman <rusanov_roman@icloud.com>
2018-06-09 11:00:34 +0300

    Exercise(Create dynamic list based on array) complete send mentor to code review.

--- 2.Level-Junior/Chapter001_Collection_Pro/src/test/java/ru/rrusanov/dynamicListBasedArray/DynamicListTest.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/test/java/ru/rrusanov/dynamicListBasedArray/DynamicListTest.java
@@ -1,25 +1,61 @@
 package ru.rrusanov.dynamicListBasedArray;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import static org.hamcrest.core.Is.is;
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 07.06.2018
+ *
+ * Test DynamicList.java.
+ */
 public class DynamicListTest {
-
+    /**
+     * The field contain instance of tested class.
+     */
+    private DynamicList<Integer> dynamicList = new DynamicList<>();
+    /**
+     * Section execute before each tests.
+     */
     @Before
-    public void setUp() throws Exception {
+    public void before() {
+        dynamicList.add(1);
+        dynamicList.add(2);
+        dynamicList.add(3);
     }
-
+    /**
+     * Test add and get methods.
+     */
     @Test
-    public void add() {
+    public void whenAddElementsThenReturnThem() {
+        Assert.assertThat(dynamicList.get(0), is(1));
+        Assert.assertThat(dynamicList.get(1), is(2));
+        Assert.assertThat(dynamicList.get(2), is(3));
     }
-
+    /**
+     * Test Iterator.
+     */
     @Test
-    public void get() {
+    public void whenIteratorCreateWhenNextReturnElementsSequentially() {
+        Iterator<Integer> iterator = dynamicList.iterator();
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertThat(iterator.next(), is(1));
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertThat(iterator.next(), is(2));
+        Assert.assertThat(iterator.next(), is(3));
+        Assert.assertFalse(iterator.hasNext());
     }
-
-    @Test
-    public void iterator() {
+    /**
+     * Test fail-fast behavior.
+     */
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenCollectionChangedThenTrowException() {
+        Iterator<Integer> iterator = dynamicList.iterator();
+        dynamicList.add(4);
+        iterator.hasNext();
     }
 }
