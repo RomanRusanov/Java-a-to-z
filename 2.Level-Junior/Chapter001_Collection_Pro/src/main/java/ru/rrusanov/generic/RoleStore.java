ca7bdba94a898cee906471640e8a2cff0fc82ad2
RomanRusanov <rusanov_roman@icloud.com>
2018-06-21 13:46:35 +0300

    Exercise (Implements class Store<T extends Base>) refactor with mentor coments.

--- 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/generic/RoleStore.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/generic/RoleStore.java
@@ -7,35 +7,4 @@ package ru.rrusanov.generic;
  * Collection Role class.
  */
 public class RoleStore extends AbstractStore<Role> implements Store<Role> {
-    /**
-     * Method add model to collection.
-     *
-     * @param model to add.
-     */
-    @Override
-    public void add(Role model) {
-        super.add(model);
-    }
-    /**
-     * Method replace new value model to existing id.
-     *
-     * @param id    unique to find model.
-     * @param model new model to replace.
-     * @return if operation success return true, otherwise false.
-     */
-    @Override
-    public boolean replace(String id, Role model) {
-        return super.replace(id, model);
-    }
-    /**
-     * Method find in collection model by id string.
-     *
-     * @param id string to find.
-     * @return if find return that model.
-     */
-    @Override
-    public Role findById(String id) throws NotFoundException {
-        return super.findById(id);
-    }
-
 }
--- 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/generic/UserStore.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/generic/UserStore.java
@@ -8,40 +8,4 @@ package ru.rrusanov.generic;
  * Collection User class.
  */
 public class UserStore extends AbstractStore<User> implements Store<User> {
-    /**
-     * Default constructor.
-     */
-    public UserStore() {
-        super();
-    }
-    /**
-     * Method add model to collection.
-     *
-     * @param model to add.
-     */
-    @Override
-    public void add(User model) {
-        super.add(model);
-    }
-    /**
-     * Method replace new value model to existing id.
-     *
-     * @param id    unique to find model.
-     * @param model new model to replace.
-     * @return if operation success return true, otherwise false.
-     */
-    @Override
-    public boolean replace(String id, User model) {
-        return super.replace(id, model);
-    }
-    /**
-     * Method find in collection model by id string.
-     *
-     * @param id string to find.
-     * @return if find return that model.
-     */
-    @Override
-    public User findById(String id) throws NotFoundException {
-        return super.findById(id);
-    }
 }
