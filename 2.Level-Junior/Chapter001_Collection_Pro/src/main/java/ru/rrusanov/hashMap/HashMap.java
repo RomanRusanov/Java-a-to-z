fdc015183c246ea303ba21c87d57e4b4ad84cf5c
RusanovRoman <rusanov_roman@icloud.com>
2018-07-03 16:22:10 +0300

    New exercise (Implement HashMap collection.) final version.

--- 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/hashMap/HashMap.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/main/java/ru/rrusanov/hashMap/HashMap.java
@@ -1,6 +1,6 @@
 package ru.rrusanov.hashMap;
-import java.util.*;
-
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 /**
  * @author Roman Rusanov
  * @version 0.1
@@ -11,23 +11,47 @@ import java.util.*;
  * The class describes collection with classic HashMap without collision handling. If exist collision put return false.
  */
 public class HashMap<K, V> implements Iterable<Entry<K, V>> {
-
+    /**
+     * The field contain entry(key, value) elements.
+     */
     private Entry<K, V>[] collection;
-
+    /**
+     * The field contain current value of collection size.
+     */
     private int size;
-
+    /**
+     * The field contain current value of entry elements in collection.
+     */
     private int elementCounter;
-
+    /**
+     * The default constructor.
+     * @param size initial size of collection.
+     */
     public HashMap(int size) {
         this.collection = new Entry[size];
         this.size = this.collection.length;
         this.elementCounter = 0;
     }
-
+    /**
+     * The method return hash value for key.
+     * @param key The value of the key for which you want to compute a hash.
+     * @return hash value.
+     */
     public int hash(K key) {
         return (key == null ? 0 : key.hashCode() % size);
     }
-
+    /**
+     * The method return size of collection.
+     * @return size value.
+     */
+    public int getSize() {
+        return this.size;
+    }
+    /**
+     * The method resize(old length + 1) the collection field if call insert
+     * method and no more left free entry in array,
+     * and rehash all elements, because hash compute on collection length.
+     */
     public void growAndReHash() {
         Entry<K, V>[] newCollection = new Entry[this.collection.length + 1];
         this.size++;
@@ -37,17 +61,27 @@ public class HashMap<K, V> implements Iterable<Entry<K, V>> {
         }
         this.collection = newCollection;
     }
-
+    /**
+     * The method need to iterator behavior support. Remove null elements from collection field.
+     * And return copy with out null elements
+     * @return Array Entry
+     */
     public Entry<K, V>[] removeNullsElements() {
         Entry<K, V>[] newCollection = new Entry[this.elementCounter];
         int index = 0;
         for (Entry item : this.collection) {
-            if (item.getKey() != null) {
+            if (item != null && item.getKey() != null) {
                 newCollection[index++] = item;
             }
         }
         return newCollection;
     }
+    /**
+     * The method insert new pair key and value in collection. Null key value not accepted.
+     * @param key value
+     * @param value associated value with key.
+     * @return If pair added then return true, otherwise false.
+     */
     boolean insert(K key, V value) {
         if (this.elementCounter == this.size) {
             this.growAndReHash();
@@ -62,30 +96,48 @@ public class HashMap<K, V> implements Iterable<Entry<K, V>> {
         }
         return result;
     }
-
+    /**
+     * The method return value associated with passed key value.
+     * @param key value.
+     * @return associated value with key.
+     */
     V get(K key) {
         return this.collection[this.hash(key)].getValue();
     }
-
+    /**
+     * The method remove pair from collection.
+     * @param key to remove
+     * @return If pair remove then return true, otherwise false.
+     */
     boolean delete(K key) {
         boolean result = false;
         int hash = this.hash(key);
-        if (this.collection[hash].getKey() == key) {
+        if (this.collection[hash] != null && this.collection[hash].getKey() == key) {
             this.collection[hash] = null;
             this.elementCounter--;
             result = true;
         }
         return result;
     }
-
+    /**
+     * Classic iterator for collection.
+     * @return iterator instance.
+     */
     @Override
     public Iterator<Entry<K, V>> iterator() {
         return new Iterator<Entry<K, V>>() {
-
+            /**
+             * The field contain array elements entry without nulls key values.
+             */
             private Entry<K,V>[] elements = removeNullsElements();
-
+            /**
+             * The field contain counter how many elements iterator return.
+             */
             private int counter = 0;
-
+            /**
+             * The method check has more elements to iterate.
+             * @return True if exist element, otherwise false.
+             */
             @Override
             public boolean hasNext() {
                 boolean result = false;
@@ -94,7 +146,11 @@ public class HashMap<K, V> implements Iterable<Entry<K, V>> {
                 }
                 return result;
             }
-
+            /**
+             * The method return next element.
+             * @return Entry<K, V>.
+             * @throws NoSuchElementException If no more elements to iterate and call next method.
+             */
             @Override
             public Entry<K, V> next() throws NoSuchElementException {
                 if (hasNext()) {
@@ -102,25 +158,6 @@ public class HashMap<K, V> implements Iterable<Entry<K, V>> {
                 }
                 throw new NoSuchElementException("No more element to iterate!");
             }
-
-            public ArrayList<V> sortAscendingOrder() {
-                ArrayList<V> sortedValues = new ArrayList<>(this.elements.length);
-                for (Entry<K,V> item : this.elements) {
-                    sortedValues.add(item.getValue());
-                }
-                sortedValues.sort(new Comparator<V>() {
-                    @Override
-                    public int compare(V o1, V o2) {
-                        int result = 0;
-                        if (o1 > o2){
-
-                        }
-                        return result;
-                    }
-                });
-                return sortedValues;
-            }
-
         };
     }
 }
--- 2.Level-Junior/Chapter001_Collection_Pro/src/test/java/ru/rrusanov/hashMap/HashMapTest.java
+++ 2.Level-Junior/Chapter001_Collection_Pro/src/test/java/ru/rrusanov/hashMap/HashMapTest.java
@@ -2,60 +2,83 @@ package ru.rrusanov.hashMap;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.Iterator;
-
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 /**
  * @author Roman Rusanov
  * @version 0.1
  * @since 28.06.2018
-
+ *
+ * The class HashMapTest.java check behavior HashMap.
  */
 public class HashMapTest {
-
+    /**
+     * The instance of tested class.
+     */
     private HashMap<String, Integer> hashMap = new HashMap<>(2);
-
+    /**
+     * The method executes in each test.
+     */
     @Before
     public void setUp() {
         hashMap.insert("A", 1);
         hashMap.insert("B", 2);
     }
-
+    /**
+     * Test method growAndReHash.
+     */
     @Test
-    public void hash() {
+    public void whenNoMoreFreeEntryThenCollectionGrow() {
+        this.hashMap.insert("C", 3);
+        Assert.assertThat(this.hashMap.getSize(), is(3));
     }
-
+    /**
+     * Test method removeNullsElements.
+     */
     @Test
-    public void growAndReHash() {
+    public void whenCallRemoveNullsElementThenCollectionsWithoutNulls() {
+        this.hashMap.delete("A");
+        this.hashMap.removeNullsElements();
+        for (Entry<String, Integer> item : this.hashMap) {
+            Assert.assertNotNull(item);
+        }
     }
-
+    /**
+     * Test method insert.
+     */
     @Test
-    public void removeNullsElements() {
+    public void whenInsertInMapThenEntryExist() {
+        this.hashMap.insert("C", 3);
+        Assert.assertThat(this.hashMap.get("C"), is(3));
+
     }
-
+    /**
+     * Test method get.
+     */
     @Test
-    public void insert() {
-        hashMap.insert("C", 3);
-    }
-
-    @Test
-    public void get() {
+    public void whenCallGetWhenReturnValue() {
         Assert.assertThat(this.hashMap.get("A"), is(1));
         Assert.assertThat(this.hashMap.get("B"), is(2));
-        Assert.assertThat(this.hashMap.get("C"), is(3));
     }
-
+    /**
+     *
+     */
     @Test
-    public void delete() {
+    public void whenEntryDeletedThenItNotExist() {
+        Assert.assertTrue(this.hashMap.delete("A"));
+        Assert.assertFalse(this.hashMap.delete("A"));
     }
 
     @Test
     public void iterator() {
         Iterator<Entry<String, Integer>> it = hashMap.iterator();
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next().getKey(), is("A"));
-
+        assertTrue(it.hasNext());
+        assertThat(it.next().getKey(), anyOf(is("A"), is("B")));
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+        assertThat(it.next().getKey(), anyOf(is("A"), is("B")));
+        assertFalse(it.hasNext());
     }
 }
