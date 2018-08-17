cf89051cd6f6ad94229a2e22aeeb001fad131c4d
RusanovRoman <rusanov_roman@icloud.com>
2018-08-17 15:14:33 +0300

    Exercise (Concurrency UserStore) seconf commit

--- 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/threads/userStorage/UserStore.java
+++ 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/threads/userStorage/UserStore.java
@@ -2,7 +2,6 @@ package ru.rrusanov.threads.userStorage;
 
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
-
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -21,11 +20,38 @@ public class UserStore {
     public UserStore() {
         this.collection = new TreeMap<>();
     }
-    @GuardedBy("method-add()")
-    public synchronized boolean add(User user) {
+
+    public boolean add(User user) {
         boolean result = false;
         if (!this.collection.containsKey(user.getId())) {
             this.collection.put(user.getId(), user);
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean update(User user) {
+        boolean result = false;
+        return result;
+    }
+
+    public boolean delete(User user) {
+        boolean result = false;
+        if (!this.collection.containsKey(user.getId())) {
+            this.collection.remove(user.getId());
+            result = true;
+        }
+        return result;
+    }
+
+    @GuardedBy("this")
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        boolean result = false;
+        User from = this.collection.get(fromId);
+        User to = this.collection.get(toId);
+        if (from != null && to != null && from.getAmount() >= amount) {
+            from.setAmount(from.getAmount() - amount);
+            to.setAmount(to.getAmount() + amount);
             result = true;
         }
         return result;
