0d3c51b2cc07e40f882033964168c08a477e253c
RomanRusanov <rusanov_roman@icloud.com>
2018-08-29 16:34:42 +0300

    Exercise (Implements patern Producer Consumer)package.info change symbol end of line

--- 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/waitNotifyNotifyAll/producerConsumer/package-info.java
+++ 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/waitNotifyNotifyAll/producerConsumer/package-info.java
@@ -1,1 +1,11 @@
-/** * @author Roman Rusanov * @version 0.1 * @since 29.08.2018 * * The SimpleBlockingQueue.java class describe behavior collection with blocking bounded queue. *  int getNumberOfElements() The method count not nulls values in collection. *  void offer(T value) The method add value in queue. *  T pool() The method retire element from queue, and return them. */package ru.rrusanov.waitNotifyNotifyAll.producerConsumer;
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 29.08.2018
+ *
+ * The SimpleBlockingQueue.java class describe behavior collection with blocking bounded queue.
+ *  int getNumberOfElements() The method count not nulls values in collection.
+ *  void offer(T value) The method add value in queue.
+ *  T pool() The method retire element from queue, and return them.
+ */
+package ru.rrusanov.waitNotifyNotifyAll.producerConsumer;
--- 2.Level-Junior/Chapter002_Multithreading/src/test/java/ru/rrusanov/waitNotifyNotifyAll/producerConsumer/package-info.java
+++ 2.Level-Junior/Chapter002_Multithreading/src/test/java/ru/rrusanov/waitNotifyNotifyAll/producerConsumer/package-info.java
@@ -1,1 +1,9 @@
-/** * @author Roman Rusanov * @version 0.1 * @since 29.08.2018 * * The SimpleBlockingQueueTest.java class check simple blocking queue. *  void whenQueueFullThenWait() The test check behavior with blocking queue. */package ru.rrusanov.waitNotifyNotifyAll.producerConsumer;
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 29.08.2018
+ *
+ * The SimpleBlockingQueueTest.java class check simple blocking queue.
+ *  void whenQueueFullThenWait() The test check behavior with blocking queue.
+ */
+package ru.rrusanov.waitNotifyNotifyAll.producerConsumer;
