fbcc9241f73de2d35fcd40b4692074c7489a69ba
RomanRusanov <rusanov_roman@icloud.com>
2018-08-31 12:09:21 +0300

    Exercise (Providing Consumer stop) refactor code style syntax error

--- 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/waitNotifyNotifyAll/parallelSearch/package-info.java
+++ 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/waitNotifyNotifyAll/parallelSearch/package-info.java
@@ -2,7 +2,7 @@
  * @author Roman Rusanov
  * @version 0.1
  * @since 30.08.2018
- * 
+ *
  *  The SimpleBlockingQueue.java class describe behavior collection with blocking bounded queue.
  *   void offer(T value) The method add value in queue.
  *   T poll() The method retire element from queue, and return them.
