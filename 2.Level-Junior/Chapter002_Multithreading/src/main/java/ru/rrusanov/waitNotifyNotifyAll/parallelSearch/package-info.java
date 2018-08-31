4b242dd8e02df11702ca6801f34b4b481bad6d3d
RomanRusanov <rusanov_roman@icloud.com>
2018-08-31 12:03:04 +0300

    Exercise (Providing Consumer stop) add package.info

--- 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/waitNotifyNotifyAll/parallelSearch/package-info.java
+++ 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/waitNotifyNotifyAll/parallelSearch/package-info.java
@@ -2,6 +2,14 @@
  * @author Roman Rusanov
  * @version 0.1
  * @since 30.08.2018
+ * 
+ *  The SimpleBlockingQueue.java class describe behavior collection with blocking bounded queue.
+ *   void offer(T value) The method add value in queue.
+ *   T poll() The method retire element from queue, and return them.
+ *   void setProducerStop(boolean value) The method setter.
+ *   boolean getProducerStop() The method getter.
  *
+ *  The ParallelSearch.java class show behavior two threads(producer and consumer) when producer stop,
+ *   then consumer thread terminate.
  */
 package ru.rrusanov.waitNotifyNotifyAll.parallelSearch;
