29c85bdbc08e430d65f112b542afb1b599c1f8df
RusanovRoman <rusanov_roman@icloud.com>
2018-09-18 14:21:47 +0300

    Exercise(Count function in range) refactor, with mentor comments.

--- 1.Level-Trainee/Chapter004_FP_Labmda_StreamAPI/src/test/java/lambda/countFunction/CountFunctionTest.java
+++ 1.Level-Trainee/Chapter004_FP_Labmda_StreamAPI/src/test/java/lambda/countFunction/CountFunctionTest.java
@@ -1,5 +1,8 @@
 package lambda.countFunction;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 /**
  * @author Roman Rusanov
@@ -18,17 +21,13 @@ public class CountFunctionTest {
      */
     @Test
     public void diapason() {
-
         List<Double> linear = this.function.diapason(1, 10, (x) -> 2D * x + 4D);
+        Assert.assertEquals(Arrays.asList(6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0), linear);
 
         List<Double> quad = this.function.diapason(1, 10, (x) -> 2D * Math.pow(x, 2) + 4D);
+        Assert.assertEquals(Arrays.asList(6.0, 12.0, 22.0, 36.0, 54.0, 76.0, 102.0, 132.0, 166.0), quad);
 
         List<Double> logarithm = this.function.diapason(1, 10, Math::log);
-
-        System.out.println("Linear function " + linear);
-
-        System.out.println("Quad function " + quad);
-
-        System.out.println("Logarithm function " + logarithm);
+        Assert.assertEquals(Arrays.asList(0.0, 0.6931471805599453, 1.0986122886681098, 1.3862943611198906, 1.6094379124341003, 1.791759469228055, 1.9459101490553132, 2.0794415416798357, 2.1972245773362196), logarithm);
     }
 }
