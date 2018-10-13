276b15702678f40cd2163f86fbf3e1c8f17c0f25
RomanRusanov <rusanov_roman@icloud.com>
2018-10-13 10:00:33 +0300

    Exercise(game Bomberman) refactor with mentor comments.

--- 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/bomberman/Board.java
+++ 2.Level-Junior/Chapter002_Multithreading/src/main/java/ru/rrusanov/bomberman/Board.java
@@ -31,11 +31,13 @@ public class Board implements Runnable {
      */
     public boolean move(Cell source, Cell dist) {
         boolean result = false;
-        if (!this.board[dist.getX()][dist.getY()].isLocked()
-                && this.board[dist.getX()][dist.getY()].tryLock()
-        ) {
-            this.board[source.getX()][source.getY()].unlock();
-            result = true;
+        try {
+            if (this.board[dist.getX()][dist.getY()].tryLock(500, TimeUnit.MILLISECONDS)) {
+                this.board[source.getX()][source.getY()].unlock();
+                result = true;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return result;
     }
@@ -47,36 +49,19 @@ public class Board implements Runnable {
         Cell positionHero = new Cell(hero.getPosition());
         Cell positionTryCell = this.hero.move();
         boolean turnSuccess = false;
-        if (this.move(positionHero, positionTryCell)) {
-            this.hero.setPosition(positionTryCell);
-            //wait one second.
-            try {
-                this.board[this.hero.getPosition().getX()][this.hero.getPosition().getY()].newCondition().await(1000, TimeUnit.MILLISECONDS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                while (!turnSuccess) {
-                    if (!this.board[positionTryCell.getX()][positionTryCell.getY()].isLocked()
-                            && this.board[positionTryCell.getX()][positionTryCell.getY()].tryLock(500, TimeUnit.MILLISECONDS)
-                    ) {
-                        this.board[positionHero.getX()][positionHero.getY()].unlock();
-                        this.hero.setPosition(positionTryCell);
-                        turnSuccess = true;
-                        //wait one second.
-                        try {
-                            this.board[this.hero.getPosition().getX()][this.hero.getPosition().getY()].newCondition().await(1000, TimeUnit.MILLISECONDS);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        this.hero.applyChangeDirection();
-                        positionTryCell = this.hero.move();
-                    }
+        while (!turnSuccess) {
+            if (this.move(positionHero, positionTryCell)) {
+                this.hero.setPosition(positionTryCell);
+                turnSuccess = true;
+                //wait one second.
+                try {
+                    this.board[this.hero.getPosition().getX()][this.hero.getPosition().getY()].newCondition().await(1000, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } else {
+                this.hero.applyChangeDirection();
+                positionTryCell = this.hero.move();
             }
         }
     }
