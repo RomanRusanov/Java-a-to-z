609e40daedc849cd34531a6a7d4ae839eacde62d
RusanovRoman <rusanov_roman@icloud.com>
2019-02-06 15:56:44 +0300

    New exercise(Outer join) 1 commit.

--- 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/Outer_join/outerJoin.sql
+++ 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/Outer_join/outerJoin.sql
@@ -1,54 +1,95 @@
 create database cars;
 \c cars;
 
-create table car
-(
-  id   serial primary key,
-  name varchar(200)
-);
-
 create table body
 (
   id   serial primary key,
-  name varchar(200),
+  name varchar(100),
   body_type varchar(100),
-  car_id int not null,
-  constraint body_car_id_fk
-  foreign key (car_id)
-  references car(id)
+  color varchar(50),
+  number_seat int,
+  number_doors int
 );
 
 create table engine
 (
   id   serial primary key,
-  name varchar(200),
-  car_id int not null,
-  constraint engine_car_id_fk
-  foreign key (car_id)
-  references car(id)
+  name varchar(100),
+  en_type varchar(100),
+  horse_power int,
+  car_id int not null
 );
 
 create table transmission
 (
   id   serial primary key,
-  name varchar(200),
-  car_id int not null,
-  constraint transmission_car_id_fk
-  foreign key (car_id)
-  references car(id)
+  name varchar(100),
+  tr_type varchar(100),
+  num_speed int,
+  car_id int not null
 );
 
-insert into body ()
-values (
-       );
+create table car
+(
+  car_id   serial primary key,
+  name varchar(100),
+  body_id int,
+  engine_id int,
+  transmission_id int,
+  constraint car_body_id_fk
+    foreign key (body_id)
+      references body(id),
+  constraint car_engine_id_fk
+    foreign key (engine_id)
+      references engine(id),
+  constraint car_transmission_if_fk
+    foreign key (transmission_id)
+      references transmission(id)
+);
 
-insert into engine ()
-values (
-       );
-insert into transmission ()
-values (
-       );
+insert into body (name, body_type, color, number_seat, number_doors)
+values ('B1', 'sedan', 'white', 4, 4),
+       ('B2', 'coupe', 'red', 4, 2),
+       ('B3', 'sport-cope', 'black', 2, 2),
+       ('B4', 'hatchback', 'grey', 4, 5),
+       ('B5', 'krosover', 'white', 4, 5)
+;
 
-insert into car ()
-values (
-       );
+insert into engine (name, en_type, horse_power)
+values ('E1', 'gasoline', 150),
+       ('E2', 'gasoline', 70),
+       ('E3', 'gasoline', 350),
+       ('E4', 'diesel', 140),
+       ('E5', 'diesel', 120),
+       ('E6', 'diesel', 230),
+       ('E7', 'diesel', 180),
+       ('E8', 'electric', 150),
+       ('E9', 'electric', 190),
+       ('E10', 'electric', 50)
+;
+
+insert into transmission (name, tr_type, num_speed)
+values ('T1', 'Robot', 9),
+       ('T2', 'GidroAvtomat', 4),
+       ('T3', 'Variator', 1),
+       ('T4', 'Robot', 6),
+       ('T5', 'GidroAvtomat', 6),
+       ('T6', 'Mechanical', 5)
+;
+
+insert into car (name, body_id, engine_id, transmission_id)
+values ('Lada Granta', 1, 1, 2),
+       ('Audi A4', 4, 4, 3),
+       ('Porsche Boxter', 3, 3, 6),
+       ('Volkswagen Polo', 1, 2, 5),
+       ('Volkswagen Tiguan', 5, 4, 1),
+       ('Renault Kaptur', 5, 5, 3)
+;
+--Query
+select c.name, b.body_type as "body", e.en_type as "engine", t.tr_type as "transmission" from car as c
+      left join body as b on c.body_id = b.id
+      left join engine as e on c.engine_id = e.id
+      left join transmission as t on c.transmission_id = t.id
+      order by c.name;
+
+select b.body_type as "body type not used" from body as b left join car as c on b.id = c.body_id where c.body_id is null;
