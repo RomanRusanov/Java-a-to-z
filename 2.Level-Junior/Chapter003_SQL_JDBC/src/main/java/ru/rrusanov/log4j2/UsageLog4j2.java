de5405dc172b2ee7c0a68d755371ce2db4a51f37
RusanovRoman <rusanov_roman@icloud.com>
2019-02-07 16:09:59 +0300

    Added dependency to pom.xml and log4j2.xml configuration.

---
+++ 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/log4j2/UsageLog4j2.java
@@ -0,0 +1,23 @@
+package ru.rrusanov.log4j2;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 07.02.2019
+ * <p>
+ * The class .
+ */
+public class UsageLog4j2 {
+
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j2.class.getName());
+
+    public static void main(String[] args) {
+
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
