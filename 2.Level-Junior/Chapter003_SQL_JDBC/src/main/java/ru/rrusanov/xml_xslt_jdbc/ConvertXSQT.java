ece4e166bb1e0ec0f01a31cd734a02d39a716f76
RusanovRoman <rusanov_roman@icloud.com>
2019-03-15 14:59:39 +0300

    Exercise(XML_XSLT_JDBC optimization) Refactor ConvertXSQT add file scheme.xsl.

--- 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/xml_xslt_jdbc/ConvertXSQT.java
+++ 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/xml_xslt_jdbc/ConvertXSQT.java
@@ -8,47 +8,33 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class ConvertXSQT {
 
     public void convert(File source, File dest, File scheme) {
-
-    }
-
-    public static void main(String[] args) throws TransformerException {
-        String xsl = "<?xml version=\"1.0\"?>\n" +
-                "<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" version=\"1.0\">\n" +
-                "<xsl:template match=\"/\">\n" +
-                "<entries>" +
-                "   <xsl:for-each select=\"entries/entry\">\n" +
-                "       <entry>" +
-                "           <xsl:attribute name=\"field\">" +
-                "               <xsl:value-of select=\"field\"/>" +
-                "           </xsl:attribute>" +
-                "       </entry>\n" +
-                "   </xsl:for-each>\n" +
-                " </entries>\n" +
-                "</xsl:template>\n" +
-                "</xsl:stylesheet>\n";
-
-        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-                "<entries>\n" +
-                "   <entry>\n" +
-                "       <field>1</field>\n" +
-                "   </entry>\n" +
-            "       <entry>\n" +
-                "       <field>2</field>\n" +
-                "   </entry>\n" +
-                "</entries>";
         TransformerFactory factory = TransformerFactory.newInstance();
-        Transformer transformer = factory.newTransformer(
-                new StreamSource(
-                        new ByteArrayInputStream(xsl.getBytes()))
-        );
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.transform(new StreamSource(
-                        new ByteArrayInputStream(xml.getBytes())),
-                new StreamResult(System.out)
-        );
+        try {
+            Transformer transformer = factory.newTransformer(new StreamSource(new FileInputStream(scheme)));
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(new StreamSource(
+                            new ByteArrayInputStream(xml.getBytes())),
+                    new StreamResult(System.out)
+            );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
+//    String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+//            "<entries>\n" +
+//            " <entry>\n" +
+//            " <field>1</field>\n" +
+//            " </entry>\n" +
+//            " <entry>\n" +
+//            " <field>2</field>\n" +
+//            " </entry>\n" +
+//            "</entries>";
 }
