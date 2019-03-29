351b16e1e38a7d348b4238b70fa7d52104536d2e
RusanovRoman <rusanov_roman@icloud.com>
2019-03-29 14:31:19 +0300

    New exercise(Parser Sql.ru) Jsoup, try to get list of topics.

--- 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/sqlruParser/Parser.java
+++ 2.Level-Junior/Chapter003_SQL_JDBC/src/main/java/ru/rrusanov/sqlruParser/Parser.java
@@ -1,7 +1,10 @@
 package ru.rrusanov.sqlruParser;
 
+import com.sun.xml.internal.bind.v2.runtime.output.NamespaceContextImpl;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -15,7 +18,11 @@ import java.io.IOException;
 public class Parser {
     public static void main(String[] args) throws IOException {
         Document document = Jsoup.connect("http://www.sql.ru/forum/job").get();
-        System.out.println(document);
+//        Element pageContainer = document.getElementById("page-container");
+//        Element contentWrapperForum = pageContainer.getElementById("content-wrapper-forum");
+//        Elements table = contentWrapperForum.getElementsByTag("table");
+//        Elements forumTable = table.next(".forumTable");
+        Elements t = document.getElementsByAttributeValue("class", "postslisttopic");
     }
 
 }
