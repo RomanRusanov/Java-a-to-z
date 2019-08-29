c3887633add5f967483f067d1a70ca2c65f416d0
RusanovRoman(WinHP) <rusanov_roman@icloud.com>
2019-08-29 14:55:55 +0300

    Exercise (SimpleGenerator) complete send mentor to code review.

--- 2.Level-Junior/Chapter004_OOD/src/main/java/ru/rrusanov/TDD/package-info.java
+++ 2.Level-Junior/Chapter004_OOD/src/main/java/ru/rrusanov/TDD/package-info.java
@@ -3,6 +3,10 @@
  * @version 0.1
  * @since 4.08.2019
  *
- *
+ * Class SimpleGenerator.java The class use RegExp check input sting.
+ *  public void addPair(String key, String value) The method add par to collection.
+ *  public void setInputString(String inputString) The method Setter for filed.
+ *  public String process() The method main process find in input string keys values and replace them.
+ *  public String checkKey(String key) The method check if key exist in collection.
  */
 package ru.rrusanov.TDD;
--- 2.Level-Junior/Chapter004_OOD/src/test/java/ru/rrusanov/TDD/package-info.java
+++ 2.Level-Junior/Chapter004_OOD/src/test/java/ru/rrusanov/TDD/package-info.java
@@ -3,6 +3,14 @@
  * @version 0.1
  * @since 4.08.2019
  *
- *
+ * Class SimpleGeneratorTest.java test behavior SimpleGenerator.java class.
+ *  public void setUp() The method executes before each test.
+ *  public void whenPassStringWhitKeysWhenKeysReplaysInString() The test check behavior process method. Use 2 pairs.
+ *  public void whenPassStringWhitKeysWhenKeysReplaysInString2() The test check behavior process method.
+ *   Use 1 repeated pair.
+ *  public void whenKeysNotExistInMapWhenMethodThrowException() The test check if map not contain match key,
+ *   must throw exception.
+ *  public void whenKeysNotExistInInputStringWhenMethodThrowException() The test check if map contain not used keys,
+ *   must throw exception.
  */
 package ru.rrusanov.TDD;
