c913d03f2439342ef7cc06c03741db00d4ee8436
RomanRusanov <roman9628@gmail.com>
2020-09-17 12:57:27 +0300

    The exercise thread start complete send mentor to code review.

---
+++ 3.Level-Middle/Multithreading/Threads/src/main/java/startthread/ConcurrentOutput.java
@@ -0,0 +1,23 @@
+package startthread;
+
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 17.09.2020
+ * email roman9628@gmail.com
+ * The class demonstrate that two threads start in random priority.
+ */
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+
+    }
+}
