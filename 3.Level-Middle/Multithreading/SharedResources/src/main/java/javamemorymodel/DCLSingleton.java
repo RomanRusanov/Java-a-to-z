de006ef9c99395072de1ac36659e2fa7f9376724
RomanRusanov <roman9628@gmail.com>
2020-09-21 12:58:36 +0300

    The exercise java memory model complete send mentor to code review.

---
+++ 3.Level-Middle/Multithreading/SharedResources/src/main/java/javamemorymodel/DCLSingleton.java
@@ -0,0 +1,35 @@
+package javamemorymodel;
+
+/**
+ * @author Roman Rusanov
+ * @version 0.1
+ * @since 21.09.2020
+ * email roman9628@gmail.com
+ * 1. Ниже приведен код синглтона - double check locking.
+ * 2. Исправьте в нем ошибку. Текстом в комментарии напишите,
+ *    почему возникает ошибка.
+ * 3. Загрузите код в репозиторий. Оставьте ссылку на коммит.
+ */
+public final class DCLSingleton {
+    /**
+     * The field contain instance.
+     */
+    private static volatile DCLSingleton inst;
+
+    /**
+     * The method generate instance of class.
+     * @return DCLSingleton.
+     */
+    public static synchronized DCLSingleton instOf() {
+        if (inst == null) {
+            inst = new DCLSingleton();
+        }
+        return inst;
+    }
+
+    /**
+     * The default constructor.
+     */
+    private DCLSingleton() {
+    }
+}
